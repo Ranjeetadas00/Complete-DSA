9c970705df1117bcd3f00b014f54a7c96136b76d
Ranjeeta Das <100126029+Ranjeetadas00@users.noreply.github.com>
2025-08-28 09:06:42 +0000

    basics

--- C++/Part1.cpp
+++ C++/Part1.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
 
+/*
     int x = 10; //range is -10^9 to 10^9
     long y = 123456789; //range is -10^12 to 10^12
     long long z = 123456123456123456 ;//range is -10^18 to 10^18
@@ -21,6 +22,56 @@ cout <<ch;
 
 // DataTypes
 // int , long, long long, float, double, string , char
+
+*/
+
+// // switch cases use
+// int day;
+// cin>>day;
+// switch (day)
+// {
+// case 1:
+// cout<<"Monday";
+//     break;
+//     case 2:
+// cout<<"Tuesday";
+//     break;
+//     case 3:
+// cout<<"Wednesday";
+//     break;
+//     case 4:
+// cout<<"Thursday";
+//     break;
+//     case 5:
+// cout<<"Friday";
+//     break;
+//     case 6:
+// cout<<"Saturday";
+//     break;
+//     case 7:
+// cout<<"Sunday";
+//     break;
+
+// default:
+// cout<<"Not valid Day";
+//     break;
+// }
+
+
+//Arrays And String BAsics
+// int a[5]= {5,6,7,8};
+// cout <<a[1]<<a[2]<<endl<<a[3];
+
+// int arr[3];
+// cin>>arr[0]>>arr[1]>>arr[2];
+// cout <<arr[1]<<arr[2]<<endl<<arr[0];
+
+// string s = "Ranjeeta Das";
+// cout<<s.size(); //12
+// cout<<s[1]<<s[2]; //an
+
+// loops in C++
+// for , while , do while
 
     return 0;
 }
